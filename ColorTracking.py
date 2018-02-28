b50ae99021056e4d2a426d8ff174f77d000894c1
snielsen221b <sophianielsen221b@gmail>
2018-02-27 21:32:11 -0500

    data pickled

--- ColorTracking.py
+++ ColorTracking.py
@@ -2,17 +2,18 @@
 #python color_tracking.py
 
 #https://www.pyimagesearch.com/2015/09/21/opencv-track-object-movement/
- 
+
 # import the necessary packages
 from collections import deque
 import numpy as np
 import argparse
 import imutils
 import cv2
-import urllib #for reading image from 
+import urllib #for reading image from
 import time
- 
- 
+import pickle
+
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
@@ -20,26 +21,26 @@ ap.add_argument("-v", "--video",
 ap.add_argument("-b", "--buffer", type=int, default=64,
     help="max buffer size")
 args = vars(ap.parse_args())
- 
+
 # define the lower and upper boundaries of the colors in the HSV color space
 lower = {'blue':(97, 100, 117),'yellow':(23, 140, 119)} #assign new item lower['blue'] = (93, 10, 0)
 upper = {'blue':(117,255,255),'yellow':(54,255,255)}
- 
+
 # define standard colors for circle around the object
 colors = {'blue':(255,0,0),'yellow':(0, 200, 217)}
 
-#Tracking center and time in an array 
+#Tracking center and time in an array
 
-centers = [] 
- 
+centers = []
+
 #pts = deque(maxlen=args["buffer"])
- 
+
 # if a video path was not supplied, grab the reference
 # to the webcam
 if not args.get("video", False):
     camera = cv2.VideoCapture(0)
-   
- 
+
+
 # otherwise, grab a reference to the video file
 else:
     camera = cv2.VideoCapture(args["video"])
@@ -51,11 +52,11 @@ while True:
     # then we have reached the end of the video
     if args.get("video") and not grabbed:
         break
- 
+
     # resize the frame, blur it, and convert it to the HSV
     # color space
     frame = imutils.resize(frame, width=600)
- 
+
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
     #for each color in dictionary check object in frame
@@ -67,23 +68,23 @@ while True:
         mask = cv2.inRange(hsv, lower[key], upper[key])
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-               
+
         # find contours in the mask and initialize the current
         # (x, y) center of the ball
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_SIMPLE)[-2]
         center = None
-       
+
         # only proceed if at least one contour was found
         if len(cnts) > 0:
-            for b in cnts: 
+            for b in cnts:
                 # find the largest contour in the mask, then use
                 # it to compute the minimum enclosing circle and
                 # centroid
                 c = max(cnts, key=cv2.contourArea)
                 ((x, y), radius) = cv2.minEnclosingCircle(c)
-                M = cv2.moments(c)   
-                t = time.clock() 
+                M = cv2.moments(c)
+                t = time.clock()
 
                 # only proceed if the radius meets a minimum size. Correct this value for your obect's size
                 if radius > 0.5:
@@ -91,25 +92,29 @@ while True:
                     # then update the list of tracked points
                     cv2.circle(frame, (int(x), int(y)), int(radius), colors[key], 2)
                     cv2.putText(frame,key + " ball", (int(x-radius),int(y-radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.6,colors[key],2)
-                    
+
                     center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-                    
-                    centers.append([center, t])
+
+                    centers.append([key, center, t])
+                    # save centers data in test.txt file
+                    with open("test.txt", "wb") as fp:
+                        pickle.dump(centers, fp)
+                    print(centers)
 
 
-    for w in centers[i,:]:
-        
+    # for w in centers[i,:]:
+    #     print(w)
 
-     
+
     # show the frame to our screen
     cv2.imshow("Frame", frame)
-   
+
     key = cv2.waitKey(1) & 0xFF
 
     # if the 'q' key is pressed, stop the loop
     if key == ord("q"):
         break
- 
+
 # cleanup the camera and close any open windows
 camera.release()
 cv2.destroyAllWindows()
